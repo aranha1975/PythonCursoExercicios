db4c5cb89204189639abf4bfbc49fc08475645d1
aranha1975 <aranha1975@gmail.com>
2024-04-20 10:56:46 -0300

    Ex089 terminado

--- Ex089 – Boletim com listas compostas.py
+++ Ex089 – Boletim com listas compostas.py
@@ -12,6 +12,21 @@ while True:
     parar=str(input('Deseja Continuar? [S/N]: '))
     if parar in 'Nn':
         break
-print(dados)
-print(lista)
-#tô tentando descobrir como ele numerou cada nome no boletim
+print('='*23)
+print(f'{"BOLETIM ELETRÔNICO":^23}')
+print('='*23)
+print(f'{"Nº":<5}{"NOME":<12}{"MÉDIA":>5}')
+print('-'*23)
+for c in range(len(lista)):
+    print(f'{c+1:<5}', end='')
+    print(f'{lista[c][0]:<12}',end='')
+    print(f'{(lista[c][1]+lista[c][2])/2:>5}')
+print('-'*60)
+while True:
+    aluno = int(input('Digite nº do aluno para ver as notas dele ou 999 para parar: '))
+    if aluno == 999:
+        break
+    else:
+        print(f'As notas do aluno {lista[aluno-1][0]} são {lista[aluno-1][1]} e {lista[aluno-1][2]}')
+        print('-'*60)
+print('Obrigado por consultar nosso BOLETIM ELETRÔNICO')
